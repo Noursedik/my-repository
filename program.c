262a7a5e9571db27646248a8bd1d3516ff57eea2
Noursedik <146179829+Noursedik@users.noreply.github.com>
2023-09-27 17:54:33 +0200

    firstattempt

--- program.c
+++ program.c
@@ -1,11 +1,31 @@
-#include <math.h>
 #include <stdio.h>
+#include<math.h>
+#define min_amount 2 // The minimum amount of product
+#define quantity 10 // The quantity of the products at the start 
+/* Declaring the variables to be used in the program*/
+static int admin_password=67678;                                                           
+static char item1='A', item2='B', item3='C'; 
+static float price1=2.0, price2=3.5, price3=1.5, total_amount;
+
+/* Functions Prototypes*/
+void display_itmes(void);
 
 int main()
 {
-printf("Wecome to the Vending Machine\n");
-printf("You have 3 main options\n");
-
-
-
+    int action;
+    printf("Welcome to the Vending Machine\n");
+    printf("Here are the options, please choose one:\n");
+    printf("1-Purchase an Item\n2-Admin Mode\n3-Exit\n");
+    scanf("%d",&action);
 }
+void display_itmes(void)// To diplay all details about the available items
+{
+    printf("The items availabe are:\n");
+    printf("%c, code: 1, price %-8.2f\n",item1,price1);
+    printf("%c, code: 2, price %-8.2f\n",item2,price2);
+    printf("%c, code: 3, price %-8.2f\n",item3,price3);
+    printf("Please select an item code or press 0 to cancel purchase\n");
+    int item_code;
+    scanf("%d",&item_code);
+}
+   
