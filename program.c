efeb2715178059cd393da7609013c8d915636199
Noursedik <146179829+Noursedik@users.noreply.github.com>
2023-11-02 11:20:48 +0200

    NourSedikQ2Lab2

--- program.c
+++ program.c
@@ -42,6 +42,7 @@ int main() {
             main();
             break;
         case 4:
+            printf("Thank you for using our system\n");
             return(1);
         default:
             printf("Invalid Choice.\n");
@@ -54,19 +55,19 @@ void enrol() {
     int id;
     printf("Enter Student ID: ");//allowing user to enter student Id
     scanf("%d", &id);
-    int index = -1;
+    int found = -1;
     //Check in the student Id aray if this student already exists
     for (int i = 0; i < numOfStudents; i++) {
         if (studentList[i].StudentID == id) {
-            index = i;
+            found = i;
             break;
         }
     }
     //If student doesn't exist allow user to enter student details as per the data structure
-    if (index == -1) {
+    if (found == -1) {
         for (int i = 0; i < numOfStudents; i++) {
             if (studentList[i].StudentID == 0) {
-                index = i;
+                found = i;
                 studentList[i].StudentID = id;
                 printf("Enter First Name: ");
                 scanf("%s", studentList[i].FirstName);
@@ -85,19 +86,19 @@ void enrol() {
                 /*Calculating the total marks if all subject marks are inputed*/
                 float total = 0;
                 for (int j = 0; j < numOfSubjects; j++) {
-                    if (studentList[i].SubjectMarks[j] != -1) {
-                        total += studentList[i].SubjectMarks[j];
+                    if (studentList[i].SubjectMarks[j] == -1) {
+                        total=-1;
+                        break;
                     }
                     else {
-                        total=0;
-                        break;
+                        total += studentList[i].SubjectMarks[j];
                     }
                 }
                 /*Printing the grade for the student depending on their Average Mark*/
-                if (total == 0) {
+                if (total ==-1) {
                     studentList[i].AggregateMarks = -1;
                     strcpy(studentList[i].Grade, "undefined");
-                } else {
+                }else {
                     studentList[i].AggregateMarks = (int)(total/numOfSubjects);
                     if ((total/numOfSubjects) >= 85) {
                         strcpy(studentList[i].Grade, "HD");
@@ -111,21 +112,23 @@ void enrol() {
                         strcpy(studentList[i].Grade, "F");
                     }
                 }
-                break;
+                 break;   
             }
         }
-    } else {
+    }
+    else {
         printf("Student ID already exists.\n");
     }
 }
 
+
 /*Search Update Function*/
 void searchUpdate() {
-    int choice;
+    int option;
     printf("Search by:\n1. Student ID\n2. Last Name\n");//allow the user to choose to search by student Id or Last name
-    scanf("%d", &choice);
+    scanf("%d", &option);
     int index = -1;
-    if (choice == 1) {
+    if (option == 1) {
         int id;
         printf("Enter Student ID: ");
         scanf("%d", &id);
@@ -136,7 +139,7 @@ void searchUpdate() {
                 break;
             }
         }
-    } else if (choice == 2) {
+    } else if (option == 2) {
         char name[20];
         printf("Enter Last Name: ");
         scanf("%s", name);
@@ -154,7 +157,7 @@ void searchUpdate() {
     if (index == -1) {
         printf("Student not found.\n");
     } else {
-        /*Printing all student informatio as per the data structure*/
+        /*Printing all student information as per the data structure*/
         printf("Student Found:\n");
         printf("First Name: %s\n", studentList[index].FirstName);
         printf("Last Name: %s\n", studentList[index].LastName);
@@ -185,13 +188,17 @@ void searchUpdate() {
             }
             /*Calculating the total marks if all subject marks are inputed*/
             float total = 0;
-            for (int j = 0; j < numOfSubjects; j++) {
-                if (studentList[index].SubjectMarks[j] != -1) {
-                    total += studentList[index].SubjectMarks[j];
+                for (int j = 0; j < numOfSubjects; j++) {
+                    if (studentList[index].SubjectMarks[j] == -1) {
+                        total=-1;
+                        break;
+                    }
+                    else {
+                        total += studentList[index].SubjectMarks[j];
+                    }
                 }
-            }
             /*Printing the grade for the student depending on their Average Mark*/
-            if (total == 0) {
+            if (total == -1) {
                 studentList[index].AggregateMarks = -1;
                 strcpy(studentList[index].Grade, "undefined");
             } else {
@@ -215,16 +222,16 @@ void searchUpdate() {
 /*Top Student Function*/
 void topStudents() {
     //searching for the maximum average mark 
-    int max = -1;
+    int max_mark = -1;
     for (int i = 0; i < numOfStudents; i++) {
-        if (studentList[i].AggregateMarks > max) {
-            max = studentList[i].AggregateMarks;
+        if (studentList[i].AggregateMarks > max_mark) {
+            max_mark = studentList[i].AggregateMarks;
         }
     }
     printf("Top Students:\n");
     //printing all the details of all students who got the maximum average mark
     for (int i = 0; i < numOfStudents; i++) {
-        if (studentList[i].AggregateMarks == max) {
+        if (studentList[i].AggregateMarks == max_mark) {
             printf("%s %s (ID: %d) - Aggregate Marks: %d, Grade: %s\n", studentList[i].FirstName, studentList[i].LastName, studentList[i].StudentID, studentList[i].AggregateMarks, studentList[i].Grade);
         }
     }
