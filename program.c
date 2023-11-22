ce7f73a49894189ec9f76570954d4cca2694aad2
Noursedik <146179829+Noursedik@users.noreply.github.com>
2023-11-22 18:18:08 +0200

    NourSedikLab3Q1,2,3

--- program.c
+++ program.c
@@ -76,7 +76,104 @@ void recursiveShuffle(int arr[], int left, int right) {
     recursiveShuffle(arr, mid + 1, right);//recalling the function to check from middle +1 to right 
     return;
 }
+ 
+ 
+ #include <stdio.h>
+#include <stdlib.h>
+//Function to read the image pixel values from a text PGM file
+ void readPGM(char* filename, int** image, int* width, int* height) {
+  
+   char comments[256];
+   
+   FILE* file = fopen("Rain_tree.pgm", "r");
+     if (&file == NULL) {
+      printf("Error opening file!");
+         return;
+     }
 
+     //Read the PGM file header
+     char format[3];
+     fscanf(file, "%s", format);
+    if (format[0] != 'P' || format[1] != '2') {
+        printf("Invalid PGM file format!");
+        return;
+    }
+
+     //Read the image comments
+    fscanf(file, "%s", comments);
+    while (comments[0]=='#'){
+        fgets(comments,255,file);
+
+    }
+    fscanf(file, "%d %d", width, height);
+    
+    *image = (int*)malloc((*width) * (*height) * sizeof(int));
+    for (int i = 0; i < (*width) * (*height); i++) {
+        fscanf(file, "%d", &(*image)[i]);
+    }
+   
+
+    fclose(file);
+}
+
+// Function to write the image pixel values to a text PGM file
+void writePGM(char* filename, int* image, int width, int height) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file!");
+        return;
+    }
+
+    // Write the pixel values
+    for (int i = 0; i < width * height; i++) {
+        fprintf(file, "%d ", image[i]);
+    }
+
+    fclose(file);
+}
+
+// Function to perform histogram equalisation on the image
+void histogramEqualisation(int* image, int width, int height) {
+    // Calculate the histogram
+    int histogram[256] = {0};
+    for (int i = 0; i < width * height; i++) {
+        histogram[image[i]]++;
+    }
+
+    // Calculate the cumulative distribution function (CDF)
+    int cdf[256] = {0};
+    cdf[0] = histogram[0];
+    for (int i = 1; i < 256; i++) {
+        cdf[i] = cdf[i - 1] + histogram[i];
+    }
+
+    // Perform histogram equalisation
+    int totalPixels = width * height;
+    for (int i = 0; i < width * height; i++) {
+        image[i] = (cdf[image[i]] * 255) / totalPixels;
+    }
+}
+
+int main() {
+    char* inputFile = "Rain_Tree.pgm";
+    char* outputFileText = "Rain_Tree_equalized_text.pgm";
+
+    int width, height;
+    int* image;
+
+    // Read the input image
+    readPGM(inputFile,&image,&width,&height);
+
+    // Perform histogram equalisation
+    histogramEqualisation(image, width, height);
+
+    // Write the equalized image to text PGM file
+    writePGM(outputFileText, image, width, height);
+
+    // Free the allocated memory
+    free(image);
+     return 0;
+}
 /*Question 3*/
 #include<iostream>
 #include<iomanip>
